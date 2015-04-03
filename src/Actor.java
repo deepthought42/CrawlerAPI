e8e8aeb9c974a13058bbcb671411f0e374999657
Brandon Kindred <brandon.kindred@gmail.com>
2015-04-03 17:35:21 -0400

    added ability to instantiate an actor and also to have that actors run function perform actions on elements

--- src/Actor.java
+++ src/Actor.java
@@ -1,4 +1,5 @@
 import java.text.DateFormat;
+import java.util.List;
 
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
@@ -11,6 +12,10 @@ public class Actor implements Runnable {
 	private WebElement element = null;
 	private String action = null;
 	
+	public Actor(String url) {
+		this.url = url;
+	}
+
 	public Actor(String url, WebElement element, String action) {
 		this.url = url;
 		this.element = element;
@@ -25,28 +30,35 @@ public class Actor implements Runnable {
 		//get a web browser driver and open the browser to the desired url
 		//get the page
 		WebDriver driver = DiffHandler.openWithFirefox(url);
+		System.out.println("Retrieved driver");
 		String pageSrc = driver.getPageSource();
-		
+		System.out.println("Retrieved page source");
 		//create list of all possible actions
 		Page page = new Page(driver, pageSrc, url, DateFormat.getDateInstance(), false);
+		System.out.println("Built page instance.");
 		
-		int index = page.getVisibleElements(driver).indexOf(element);
-		PageElement element = page.getVisibleElements(driver).get(index);
-		ConcurrentNode<String> actionNode = new ConcurrentNode<String>("click");
+		ConcurrentNode<Page> currentPageNode = new ConcurrentNode<Page>(page);
+		List<PageElement> visibleElements = currentPageNode.data.getVisibleElements(driver);
+		System.out.println("Wrapped page instance in a graph node");
 		
-		try{
-			ActionFactory.execAction(driver, element, actionNode.data);
-		}
-		catch(StaleElementReferenceException e){
-			System.err.println("A SYSTEM ERROR WAS ENCOUNTERED WHILE ACTOR WAS PERFORMING ACTION : "+
-					actionNode.data + ". ");
-		}
-		
-		System.err.println("ACTOR EXECUTED ACTION :: " +actionNode.data);
+		for(PageElement elem : visibleElements){
+			System.out.println("Element retreived from list");
 
-		//find the element
-		//perform an action
-
+			ConcurrentNode<PageElement> element = new ConcurrentNode<PageElement>(elem);
+			currentPageNode.addOutput(element);
+			
+			System.out.println("Element wrapped in a graph node");
+			ConcurrentNode<String> actionNode = new ConcurrentNode<String>("click");
+			
+			try{
+				ActionFactory.execAction(driver, elem, actionNode.data);
+			}
+			catch(StaleElementReferenceException e){
+				System.err.println("A SYSTEM ERROR WAS ENCOUNTERED WHILE ACTOR WAS PERFORMING ACTION : "+
+						actionNode.data + ". ");
+			}
+			
+			System.err.println("ACTOR EXECUTED ACTION :: " +actionNode.data);
+		}
 	}
-
 }
--- src/EntryPoint.java
+++ src/EntryPoint.java
@@ -1,6 +1,9 @@
 
 public class EntryPoint {
 	public static void main(String[] args){
+		String url = "localhost:3000";
+		Actor actor = new Actor(url);
 		
+		actor.run();
 	}
 }

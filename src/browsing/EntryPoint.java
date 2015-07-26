87c223d843a0d323f602772bfa1a608be6a48b33
Brandon Kindred <brandon.kindred@gmail.com>
2015-07-25 23:05:42 -0400

    cleaned up EntryPoint

--- src/browsing/EntryPoint.java
+++ src/browsing/EntryPoint.java
@@ -16,22 +16,15 @@ import structs.Path;
  */
 public class EntryPoint {
 	public static void main(String[] args){
-		//graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( DB_PATH );
-		//registerShutdownHook( graphDb );
-		ResourceManagementActor resourceManager = new ResourceManagementActor(1);
+		ResourceManagementActor resourceManager = new ResourceManagementActor(5);
 		ObservableQueue<Path> pathQueue = new ObservableQueue<Path>();
 		String url = "localhost:3000";
 		System.out.print("INITIALIZING ACTOR...");
 		ExecutorService e = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 		System.out.println("TOTAL CORES AVAILABLE : "+Runtime.getRuntime().availableProcessors());
-		//e.execute(new BrowserActor(url, pageNode));
 		
 		WorkAllocationActor workAllocator = new WorkAllocationActor(pathQueue, resourceManager);
 		BrowserActor browserActor = new BrowserActor(url, pathQueue, resourceManager, workAllocator);
 		browserActor.start();
-		
-		System.out.println("Registered observer!");
-		System.out.println("THREADS STILL RUNNING AT END :: "+Thread.activeCount());
-
 	}
 }

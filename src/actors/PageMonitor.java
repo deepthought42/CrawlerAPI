e3185612d036b47d0867cb87c66f1d6843a28036
Brandon Kindred <brandon.kindred@gmail.com>
2015-07-31 21:18:53 -0400

    added PageMonitor class for keeping track of all pages for a given host

---
+++ src/actors/PageMonitor.java
@@ -0,0 +1,46 @@
+package actors;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+import browsing.Page;
+
+
+public class PageMonitor{
+	ConcurrentHashMap<String, List<Page>> hostPageMap = null;
+	
+	/**
+	 * 
+	 */
+	public PageMonitor(){
+		hostPageMap = new ConcurrentHashMap<String, List<Page>>();
+	}
+
+	/**
+	 * 
+	 * @param page
+	 * @return
+	 */
+	public Page findPage(String pageSrc, String host){		
+		List<Page> pagesForHost = hostPageMap.get(host);
+		
+		for(Page hostPage: pagesForHost){
+			if(hostPage.getSrc().equals(pageSrc)){
+				return hostPage;
+			}
+		}
+		return null;		
+	}
+	
+	/**
+	 * Adds {@link Page} to hash of host pages
+	 * 
+	 * @param page
+	 * @return true if page added successfully, otherwise false
+	 */
+	public boolean addPage(Page page){
+		String host = page.getUrl().getHost();
+		
+		return hostPageMap.get(host).add(page);
+	}
+}

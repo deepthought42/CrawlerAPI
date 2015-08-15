665f775dd8882dbe287e2b4fed127474ab8011f4
Brandon Kindred <brandon.kindred@gmail.com>
2015-08-14 22:43:11 -0400

    partially contracted IfieldGenerator and DecimalGenerator. changed random values for Decimal from long to int

--- src/generators/DecimalGenerator.java
+++ src/generators/DecimalGenerator.java
@@ -3,7 +3,7 @@ package generators;
 import java.util.Random;
 
 /**
- * 
+ * Generates Decimal values 
  * @author Brandon Kindred
  *
  */
@@ -12,9 +12,12 @@ public class DecimalGenerator implements IFieldGenerator<Double> {
 	public DecimalGenerator() {
 	}
 
+	/**
+	 * Generate double value by appending 2 integers
+	 */
 	public Double generateValue() {
 		Random random = new Random();
-		return Double.parseDouble(random.nextLong() + "." + random.nextLong());
+		return Double.parseDouble(random.nextInt() + "." + random.nextInt());
 	}
 
 }
--- src/generators/IFieldGenerator.java
+++ src/generators/IFieldGenerator.java
@@ -1,5 +1,12 @@
 package generators;
 
+/**
+ * Interface for classes used for generating assorted values.
+ * 
+ * @author Brandon Kindred
+ *
+ * @param <T>	The object type for value to be generated
+ */
 public interface IFieldGenerator<T> {
 	public T generateValue();
 }

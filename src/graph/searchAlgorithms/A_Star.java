bfe8ff5b66c2a3fbccc645b62d79e5283473b782
Brandon Kindred <brandon.kindred@gmail.com>
2015-08-24 18:27:19 -0400

    cleaned up A_Star method

--- src/graph/searchAlgorithms/A_Star.java
+++ src/graph/searchAlgorithms/A_Star.java
@@ -35,30 +35,25 @@ public class A_Star extends GraphSearch {
 		Path path = new Path();
 		int weight = 0;
 		boolean isRootFound = false;
-		//this implementation is wrong. Needs to be fixed.
 		super.frontier.put(start_idx, 0);
 		while(!isRootFound && super.frontier.size() > 0){
 			int lowest_weight = 999999;
 			int closest_index = -1;
-			System.out.println(Thread.currentThread().getName() + " -> FRONTIER SIZE :: "+ super.frontier.size());
+			
 			for(int index : super.frontier.keySet()){
 				if(super.frontier.get(index) < lowest_weight){
-					System.out.println(Thread.currentThread().getName() + " -> Found candidate for shortest path on frontier");
 					lowest_weight = super.frontier.get(index);
 					closest_index = index;
 				}
 			}
-			System.out.println(Thread.currentThread().getName() + " -> closest Index: "+closest_index);
 			super.removeNodeFromFrontier(closest_index);
 			super.visited.put(closest_index, super.frontier.get(closest_index));
 			path.add(closest_index);
 			for(Integer vertex_idx : graph.getFromIndices(closest_index)){
-				System.out.println(Thread.currentThread().getName() + " -> adding from index : "+vertex_idx);
 				super.frontier.put(vertex_idx, ++weight);
 			}
-			Vertex<?> vertex = graph.getVertices().get(closest_index);
 			
-			System.out.println(Thread.currentThread().getName() + " -> VERTEX ADDED TO PATH with idx : "+closest_index);
+			Vertex<?> vertex = graph.getVertices().get(closest_index);
 			if(vertex.isRoot() || closest_index == 0){
 				isRootFound = true;
 				break;

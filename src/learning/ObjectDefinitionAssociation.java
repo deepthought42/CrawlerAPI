655a3bc786ab67016324f7d99066e409d4a77094
Brandon Kindred <brandon.kindred@gmail.com>
2015-09-05 18:09:20 -0400

    Added in instance variables and getters and setters to ObjectDefinitionAssociation

--- src/learning/ObjectDefinitionAssociation.java
+++ src/learning/ObjectDefinitionAssociation.java
@@ -1,6 +1,8 @@
 package learning;
 
 /**
+ * Associates 2 {@link ObjectDefinition}s with each other and represents their
+ * probabilistic association with each other
  * 
  * @author Brandon Kindred
  *
@@ -10,5 +12,41 @@ public class ObjectDefinitionAssociation {
 	public ObjectDefinitionAssociation() {
 
 	}
+	
+	public long getObjectDefintion_id() {
+		return ObjectDefintion_id;
+	}
+	public void setObjectDefintion_id(long objectDefintion_id) {
+		ObjectDefintion_id = objectDefintion_id;
+	}
+
+	public long getObjectDefinition2_id() {
+		return ObjectDefinition2_id;
+	}
+
+	public void setObjectDefinition2_id(long objectDefinition2_id) {
+		ObjectDefinition2_id = objectDefinition2_id;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public long getCount() {
+		return count;
+	}
+
+	public void setCount(long count) {
+		this.count = count;
+	}
+
+	private long ObjectDefintion_id;
+	private long ObjectDefinition2_id;
+	private double weight;
+	private long count;
 
 }

3e9d4f38ee672652569f9ff49300d067b975ad19
Brandon Kindred <brandon.kindred@gmail.com>
2015-09-05 17:52:59 -0400

    added instance variables and getters and setters for ObjectDefinition

--- src/learning/ObjectDefinitionAssociation.java
+++ src/learning/ObjectDefinitionAssociation.java
@@ -1,9 +1,37 @@
 package learning;
 
+/**
+ * 
+ * @author Brandon Kindred
+ *
+ */
 public class ObjectDefinitionAssociation {
-
+	
 	public ObjectDefinitionAssociation() {
-		// TODO Auto-generated constructor stub
+
+	}
+	
+	public long getId() {
+		return id;
 	}
 
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private long id;
+	private String type;
+	private String name;
 }

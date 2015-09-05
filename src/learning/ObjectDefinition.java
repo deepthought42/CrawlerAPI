b3138d23ed70e6a441c4753cace1174f5ca1be4c
Brandon Kindred <brandon.kindred@gmail.com>
2015-09-05 17:51:00 -0400

    added classes for defining objects and their associations

---
+++ src/learning/ObjectDefinition.java
@@ -0,0 +1,9 @@
+package learning;
+
+public class ObjectDefinition {
+
+	public ObjectDefinition() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/learning/ObjectDefinitionAssociation.java
@@ -0,0 +1,9 @@
+package learning;
+
+public class ObjectDefinitionAssociation {
+
+	public ObjectDefinitionAssociation() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

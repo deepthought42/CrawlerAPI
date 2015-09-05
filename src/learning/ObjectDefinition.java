d82c3562fa4e5fa49bcbec8b2274a2c1187f3406
Brandon Kindred <brandon.kindred@gmail.com>
2015-09-05 17:54:32 -0400

    moved instance variables, getters and setters to ObjectDefinition. Added basic documentation to ObjectDefinition

--- src/learning/ObjectDefinition.java
+++ src/learning/ObjectDefinition.java
@@ -1,9 +1,39 @@
 package learning;
 
+/**
+ * Defines objects that are available to the system for learning against
+ * 
+ * @author Brandon Kindred
+ *
+ */
 public class ObjectDefinition {
 
 	public ObjectDefinition() {
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	public long getId() {
+		return id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private long id;
+	private String type;
+	private String name;
 }
--- src/learning/ObjectDefinitionAssociation.java
+++ src/learning/ObjectDefinitionAssociation.java
@@ -10,28 +10,5 @@ public class ObjectDefinitionAssociation {
 	public ObjectDefinitionAssociation() {
 
 	}
-	
-	public long getId() {
-		return id;
-	}
 
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	private long id;
-	private String type;
-	private String name;
 }

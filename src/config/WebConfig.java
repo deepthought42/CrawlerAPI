b69cc7cd324b4286a1ebd4bb3ba328555a13e42d
Brandon Kindred <brandon.kindred@gmail.com>
2016-05-28 16:16:55 -0400

    added interceptor override for cors and async support to web config

--- src/config/WebConfig.java
+++ src/config/WebConfig.java
@@ -1,7 +1,8 @@
-package api;
+package config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -11,5 +12,10 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
 
         configurer.setDefaultTimeout(1000000);
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CorsInterceptor());
     }
 }

f667d85260ea7250b0150fcec9494673d9d6b0e3
Brandon Kindred <brandon.kindred@gmail.com>
2017-09-29 23:23:07 -0400

    closed db connections opened

--- src/main/java/com/minion/actors/MemoryRegistryActor.java
+++ src/main/java/com/minion/actors/MemoryRegistryActor.java
@@ -28,7 +28,7 @@ public class MemoryRegistryActor extends UntypedActor{
 	@Override
 	public void onReceive(Object message) throws Exception {
 		if(message instanceof Message){
-
+			OrientConnectionFactory connection = new OrientConnectionFactory();
 			Message<?> acct_msg = (Message<?>)message;
 			//Retains lists of productive, unproductive, and unknown value {@link Path}s.
 			if(acct_msg.getData() instanceof Test){
@@ -38,49 +38,19 @@ public class MemoryRegistryActor extends UntypedActor{
 				}
 
 				//FramedGraphFactory factory = new FramedGraphFactory(); //(1) Factories should be reused for performance and memory conservation.
-				OrientConnectionFactory connection = new OrientConnectionFactory();
 				System.err.println("saving test : " + test + " with key : "+test.getKey());
 				TestRepository test_repo = new TestRepository();
 
 				test_repo.create(connection, test);
 				log.info("Commiting changes");
-
-
-				/*
-
-				//check if test with key already exists.
-				OrientDbPersistor persistor = new OrientDbPersistor();
-				Iterator<Vertex> vertex_iter = persistor.findVertices(test).iterator();//findByKey(test.getKey());
-				// if record already exists then create TestRecord and append it to records for test
-				if(vertex_iter.hasNext()){
-					log.info("Test already exists....adding test as record");
-					Test prev_test = (Test)vertex_iter.next();
-					TestRecord record = new TestRecord(test.getResult(), new Date(), prev_test.getPath().equals(test.getPath()));
-					prev_test.addTestRecord(record);
-
-				}
-				else{
-					log.info("Saving test for the first time");
-					Date date = new Date();
-					TestRecord record = new TestRecord(test.getResult(), date, true);
-					log.info("TEST RECORD :: "+record);
-					log.info("TEST PATH :: "+test.getPath());
-					log.info("DATE :: " + date);
-
-					test.addTestRecord(record);
-					persistor.findAndUpdateOrCreate(test);
-					//persistor.addVertexType(record, OrientDbPersistor.getProperties(record));
-					//persistor.createVertex(test);
-					//persistor.save();
-				}
-				*/
 			}
 			else if(acct_msg.getData() instanceof Path){
 				Path path = (Path)acct_msg.getData();
 				System.err.println("Saving Path : " +path + " : " + path.getKey() +" to memory Registry");
 				PathRepository path_repo = new PathRepository();
-				path_repo.create(new OrientConnectionFactory(), path);
+				path_repo.create(connection, path);
 			}
+			connection.close();
 		}
 		else unhandled(message);
 	}

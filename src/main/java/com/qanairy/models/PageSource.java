cf0de12a87bb891c6890c26587a2251846d9c2a9
Brandon Kindred <brandon.kindred@gmail.com>
2017-11-09 17:18:46 -0500

    finished page source object and repo

--- src/main/java/com/qanairy/models/PageSource.java
+++ src/main/java/com/qanairy/models/PageSource.java
@@ -5,13 +5,28 @@ package com.qanairy.models;
  *
  */
 public class PageSource {
+	private String key;
 	private String src;
 	
 	public PageSource(String src){
+		this.setKey(null);
+		this.src = src;
+	}
+	
+	public PageSource(String key, String src){
+		this.setKey(key);
 		this.src = src;
 	}
 
 	public String getSrc() {
 		return src;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
 	}
 }

c0bf16aae14f1bf0289d984b4451f31ffcf44981
Brandon Kindred <brandon.kindred@gmail.com>
2018-05-16 18:49:52 -0400

    refactored NumericRestrictionRule and added tests

--- src/main/java/com/qanairy/models/rules/NumericRestrictionRule.java
+++ src/main/java/com/qanairy/models/rules/NumericRestrictionRule.java
@@ -3,31 +3,21 @@ package com.qanairy.models.rules;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.qanairy.models.PageElement;
+import com.qanairy.persistence.PageElement;
 import com.qanairy.persistence.Rule;
 
 /**
  * Defines a {@link Rule} where the numbers 1-9 cannot appear in a given value when evaluated
  */
-public class NumericRestrictionRule implements Rule {
+public class NumericRestrictionRule extends Rule {
 
 	private String value;
+	private String key;
+	private RuleType type;
 	
 	public NumericRestrictionRule() {
-		this.value = "[0-9]*";
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public RuleType getType() {
-		return RuleType.NUMERIC_RESTRICTION;
-	}
-
-	@Override
-	public String getValue() {
-		return this.value;
+		setValue("[0-9]*");
+		setType(RuleType.NUMERIC_RESTRICTION);
 	}
 	
 	/**
@@ -39,5 +29,38 @@ public class NumericRestrictionRule implements Rule {
 
         Matcher matcher = pattern.matcher(elem.getText());
 		return !matcher.matches();
+	}
+
+	@Override
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	@Override
+	public String getKey() {
+		return this.key;
+	}
+
+	@Override
+	public void setType(RuleType type) {
+		this.type = type;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public RuleType getType() {
+		return this.type;
+	}
+
+	@Override
+	public String getValue() {
+		return this.value;
+	}
+	
+	@Override
+	public void setValue(String value) {
+		this.value = value;
 	}
 }
---
+++ src/test/java/models/rules/NumericRestrictionRuleTests.java
@@ -0,0 +1,26 @@
+package models.rules;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.qanairy.models.dao.RuleDao;
+import com.qanairy.models.dao.impl.RuleDaoImpl;
+import com.qanairy.models.rules.NumericRestrictionRule;
+import com.qanairy.persistence.Rule;
+
+/**
+ * 
+ */
+public class NumericRestrictionRuleTests {
+
+	@Test(groups="Regression")
+	public void assertRulePersists(){
+		Rule rule = new NumericRestrictionRule();
+		RuleDao dao = new RuleDaoImpl();
+		dao.save(rule);
+		
+		Rule created_rule = dao.find(rule.getKey());
+		Assert.assertTrue(created_rule.getType().equals(rule.getType()));
+		Assert.assertTrue(created_rule.getValue().equals(rule.getValue()));
+	}
+}

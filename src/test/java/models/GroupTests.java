512b49bc43507b399de4c7f75be9cee8d41a40f1
Brandon Kindred <brandon.kindred@gmail.com>
2018-05-19 02:24:06 -0400

    removed missing references

--- src/test/java/models/GroupTests.java
+++ src/test/java/models/GroupTests.java
@@ -6,7 +6,6 @@ import org.testng.annotations.Test;
 import com.qanairy.models.GroupPOJO;
 import com.qanairy.models.dao.GroupDao;
 import com.qanairy.models.dao.impl.GroupDaoImpl;
-import com.qanairy.models.dto.GroupRepository;
 import com.qanairy.persistence.Group;
 import com.qanairy.persistence.OrientConnectionFactory;
 
@@ -28,36 +27,5 @@ public class GroupTests {
 		Assert.assertEquals(created_group.getKey(),group.getKey());
 		Assert.assertTrue(created_group.getDescription().equals(group.getDescription()));
 		Assert.assertTrue(created_group.getName().equals(group.getName()));
-	}
-	
-	/**
-	 * 
-	 */
-	@Test(groups="Regression")
-	public void groupUpdateRecord(){
-		Group group = new Group("Smoke");
-		GroupRepository group_repo = new GroupRepository();
-		group.setKey(group_repo.generateKey(group));
-
-		IGroup group_record = group_repo.save(new OrientConnectionFactory(), group);
-		
-		Assert.assertTrue(group_record.getKey().equals(group.getKey()));
-		Assert.assertTrue(group_record.getDescription().equals(group.getDescription()));
-		Assert.assertTrue(group_record.getName().equals(group.getName()));
-	}
-	
-	/**
-	 * 
-	 */
-	@Test(groups="Regression")
-	public void groupFindRecord(){
-		Group group = new Group("Smoke");
-		GroupRepository group_repo = new GroupRepository();
-		group.setKey(group_repo.generateKey(group));
-		IGroup group_record = group_repo.save(new OrientConnectionFactory(), group);
-		
-		Assert.assertTrue(group_record.getKey().equals(group.getKey()));
-		Assert.assertTrue(group_record.getDescription().equals(group.getDescription()));
-		Assert.assertTrue(group_record.getName().equals(group.getName()));
 	}
 }

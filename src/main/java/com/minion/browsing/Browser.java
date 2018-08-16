7726f4f698d96e49165c2ca9c68f4afee3bb1518
Brandon Kindred <brandon.kindred@gmail.com>
2018-08-16 19:49:13 -0400

    set staging hub address

--- src/main/java/com/minion/browsing/Browser.java
+++ src/main/java/com/minion/browsing/Browser.java
@@ -59,11 +59,11 @@ public class Browser {
 	//private static final String TEST_HUB_IP_ADDRESS= "xxx.xxx.xxx.xxx";
     
 	// PRODUCTION HUB ADDRESS
-	private static final String HUB_IP_ADDRESS= "142.93.192.184:4444";
+	//private static final String HUB_IP_ADDRESS= "142.93.192.184:4444";
 	//private static final String HUB_IP_ADDRESS= "10.136.10.117:4444";
 
 	//STAGING HUB ADDRESS
-	//private static final String HUB_IP_ADDRESS="159.65.181.180:4444";
+	private static final String HUB_IP_ADDRESS="159.89.226.116:4444";
 	
     public Browser(){}
     

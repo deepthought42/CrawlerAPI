7ab227ca7a4a5a1cb242b63fff8d9c184dc5508a
Brandon Kindred <brandon.kindred@gmail.com>
2018-09-13 21:33:43 -0400

    changed browser to use staging selenium cluster,

--- src/main/java/com/minion/browsing/Browser.java
+++ src/main/java/com/minion/browsing/Browser.java
@@ -60,11 +60,11 @@ public class Browser {
 	//private static final String TEST_HUB_IP_ADDRESS= "xxx.xxx.xxx.xxx";
     
 	// PRODUCTION HUB ADDRESS
-	private static final String HUB_IP_ADDRESS= "142.93.192.184:4444";
+	//private static final String HUB_IP_ADDRESS= "142.93.192.184:4444";
 	//private static final String HUB_IP_ADDRESS= "10.136.111.115:4444";
 
 	//STAGING HUB ADDRESS
-	//private static final String HUB_IP_ADDRESS="159.89.226.116:4444";
+	private static final String HUB_IP_ADDRESS="159.89.226.116:4444";
 	
     public Browser(){}
     

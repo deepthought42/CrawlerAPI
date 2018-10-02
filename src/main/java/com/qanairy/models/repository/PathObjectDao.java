62c111ea02e0baa3151b67a95390f14849c2cae3
Brandon Kindred <brandon.kindred@gmail.com>
2018-10-02 17:10:32 -0400

    removed dead form record repo. same with path object dao

--- src/main/java/com/qanairy/models/repository/PathObjectDao.java
+++
@@ -1,8 +0,0 @@
-package com.qanairy.models.repository;
-
-import com.qanairy.models.PathObject;
-
-public interface PathObjectDao {
-	public PathObject save(PathObject path_obj);
-	public PathObject find(String key);
-}

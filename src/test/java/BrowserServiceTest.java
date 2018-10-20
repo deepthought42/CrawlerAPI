087f2e1d4195cfea9ee3bb397e4e348b877b7c15
Brandon Kindred <brandon.kindred@gmail.com>
2018-10-20 10:05:10 -0400

    renamed ide controller and set real screenshots for dummy screenshots in tests

--- src/test/java/BrowserServiceTest.java
+++ src/test/java/BrowserServiceTest.java
@@ -18,7 +18,7 @@ public class BrowserServiceTest {
 	public void verifySetMergeWorks() throws IOException{
 		Set<PageState> page_state = new HashSet<PageState>();
 		Set<ScreenshotSet> screen = new HashSet<ScreenshotSet>();
-		screen.add(new ScreenshotSet("http://qanairy.com", "chrome"));
+		screen.add(new ScreenshotSet("https://s3-us-west-2.amazonaws.com/qanairy/www.zaelab.com/pagestate::861a2edcfedf97c7ab4040a2420a6b86fe5e2db543880136567daecc6e20e8711f1f8a02586a3eca4a0aa17503ce368560516d456c244f100bd14b0df79ad896006803ca0a01edf30090275bb60e800335163d667c10480416a832009a0050e0805d061d52b010f3561f1744708f6df7d65462cf1386bd2cf2c320c5385576b31c30aa0e6ca5f7b6cc922ad5083aa8b35c5e8a15eaa08c78ca0fece91038015638f76931404c7000c86854a151e0988a2fb6c481c668b83164ba74040a9f13b09282a008d31e6a95313a4853eca2ec142c6222f1c528cd2988b63aa3a8a63ea1558c21fde256736ef5882719c644511842c9999788c389a6e0247031a033a7c67/viewport.png", "chrome"));
 		Set<PageElement> elems = new HashSet<PageElement>();
 		page_state.add(new PageState("html", "url.com", screen, elems));
 		Set<Action> actions = new HashSet<Action>();

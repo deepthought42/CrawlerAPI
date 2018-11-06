4d54191496246a6346c58035234ad3d978a2d360
Brandon Kindred <brandon.kindred@gmail.com>
2018-11-06 07:54:40 -0500

    added query to delete account

--- src/main/java/com/qanairy/models/repository/AccountRepository.java
+++ src/main/java/com/qanairy/models/repository/AccountRepository.java
@@ -26,7 +26,10 @@ public interface AccountRepository extends Neo4jRepository<Account, Long> {
 	@Query("MATCH (account:Account {username:{acct_key}})-[hd:HAS_DOMAIN]->(Domain{key:{domain_key}}) DELETE hd")
 	public void removeDomain(@Param("acct_key") String acct_key, @Param("domain_key") String domain_key);
 
-	//needs work done still to make it return all test records by month
 	@Query("MATCH (account:Account {username:{acct_key}})-[:HAS_TEST_RECORD]->(t:TestRecord) RETURN COUNT(t)")
 	public int getTestCountByMonth(@Param("acct_key") String acct_key, @Param("month") int month);
+
+	@Query("MATCH (account:Account {username:{acct_key}})-[edge]->() DELETE edge,account")
+	public int deleteAccountAndEdges(@Param("acct_key") String acct_key);
+
 }

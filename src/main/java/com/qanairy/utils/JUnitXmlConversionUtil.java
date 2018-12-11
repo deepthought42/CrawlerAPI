148ff5e6e75aaddbcee75c927c63a1a58e74390b
Brandon Kindred <brandon.kindred@gmail.com>
2018-12-11 18:54:36 -0500

    start of junit xml generation and tests

--- src/main/java/com/qanairy/utils/JUnitXmlConversionUtil.java
+++ src/main/java/com/qanairy/utils/JUnitXmlConversionUtil.java
@@ -7,26 +7,26 @@ import com.qanairy.models.enums.TestStatus;
 
 public class JUnitXmlConversionUtil {
 
-	public String convertToXml(List<TestRecord> test_record_list){
+	public static String convertToJUnitXml(List<TestRecord> test_record_list){
 		StringBuffer str_buf = new StringBuffer();
 		
-		str_buf.append("<testsuites>");
-		str_buf.append("<testsuite errors='' skipped='' tests='" +test_record_list.size()+ "' failures='' time='' timestamp=''>");
+		str_buf.append("<testsuites>\n");
+		str_buf.append("<testsuite errors='' skipped='' tests='" +test_record_list.size()+ "' failures='' time='' timestamp=''>\n");
 		
 		for(TestRecord record : test_record_list){
-			str_buf.append("<testcase name='" + record.getTest().getName()+ "' time='" + record.getRunTime() + "'> ");
+			str_buf.append("<testcase name='" + record.getTest().getName()+ "' time='" + record.getRunTime() + "'>\n");
 			
 			if(record.getPassing().equals(TestStatus.FAILING)){
-				str_buf.append("<failure message='' type='WARNING' >");
+				str_buf.append("<failure message='' type='WARNING' >\n");
 				str_buf.append("ERROR MESSAGE HERE");
-				str_buf.append("</failure>");
+				str_buf.append("</failure>\n");
 			}
 			
-			str_buf.append("</testcase>");
+			str_buf.append("</testcase>\n");
 		}
 		
-		str_buf.append("</testsuite>");
-		str_buf.append("</testsuites>");
+		str_buf.append("</testsuite>\n");
+		str_buf.append("</testsuites>\n");
 		return str_buf.toString();
 	}
 }
---
+++ src/test/java/utils/JUnitXmlConversionUtilTest.java
@@ -0,0 +1,48 @@
+package utils;
+
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.qanairy.models.Test;
+import com.qanairy.models.TestRecord;
+import com.qanairy.models.enums.TestStatus;
+import com.qanairy.utils.JUnitXmlConversionUtil;
+
+/**
+ * Unit tests for {@link JUnitXmlConversionUtil}
+ * @author brand
+ *
+ */
+public class JUnitXmlConversionUtilTest {
+
+	@Mock 
+	private Test test;
+	
+	@Mock
+	private TestRecord record;
+	
+	@Before
+	public void setup(){
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@org.junit.Test
+	public void convertToJUnitXmlTestWith1FailingRecord(){
+		List<TestRecord> records = new ArrayList<TestRecord>();
+		records.add(record);
+		
+		when(record.getTest()).thenReturn(test);
+		when(record.getRunTime()).thenReturn(10000L);
+		when(test.getName()).thenReturn("Practice test #1");
+		when(record.getPassing()).thenReturn(TestStatus.FAILING);
+
+		String xml = JUnitXmlConversionUtil.convertToJUnitXml(records);
+		System.err.println("OUTPUT ::   "+xml);
+	}
+}

316bdd7482b0c0a79bb5993f2d72a5dc487cdd37
Brandon Kindred <brandon.kindred@gmail.com>
2019-01-06 00:58:04 -0500

    added in queries for handing test history and gave instance fields better names

--- src/main/java/com/qanairy/models/repository/TestRecordRepository.java
+++ src/main/java/com/qanairy/models/repository/TestRecordRepository.java
@@ -1,9 +1,16 @@
 package com.qanairy.models.repository;
 
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import com.qanairy.models.TestRecord;
 
+@Repository
 public interface TestRecordRepository extends Neo4jRepository<TestRecord, Long> {
 	public TestRecord findByKey(@Param("key") String key);
+	
+	@Query("MATCH (tr:TestRecord{key:{key}}) SET tr.status={status} RETURN tr")
+	public TestRecord updateStatus(@Param("key") String key, @Param("status") String status);
 }
--- src/main/java/com/qanairy/models/repository/TestRepository.java
+++ src/main/java/com/qanairy/models/repository/TestRepository.java
@@ -3,7 +3,10 @@ package com.qanairy.models.repository;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
+
+import com.qanairy.models.Domain;
 import com.qanairy.models.Test;
+import com.qanairy.models.TestRecord;
 
 /**
  * 
@@ -12,6 +15,12 @@ public interface TestRepository extends Neo4jRepository<Test, Long> {
 	public Test findByKey(@Param("key") String key);
 	public Test findByName(@Param("name") String name);
 
+	@Query("MATCH (d:Domain)-[:HAS_TEST]->(t:Test{key:{key}}) RETURN d")
+	public Domain getDomain();
+	
 	@Query("MATCH p=(t:Test{key:{key}})-[:HAS_GROUP]->() RETURN p")
 	public Test getFullTestUsingKey(@Param("key") String key);
+	
+	@Query("MATCH p=(t:Test{key:{key}})-[r:HAS_TEST_RECORD]->(tr:TestRecord) RETURN tr ORDER BY tr.ran_at DESC LIMIT 1")
+	public TestRecord getMostRecentRecord(@Param("key") String key);
 }

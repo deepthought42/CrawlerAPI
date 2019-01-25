00d5d8e7c28300291434e48905d1b1b22c70a951
Brandon Kindred <brandon.kindred@gmail.com>
2019-01-24 22:11:19 -0500

    added email service implementation and bean

--- src/main/java/com/qanairy/config/ConfigService.java
+++ src/main/java/com/qanairy/config/ConfigService.java
@@ -2,11 +2,15 @@ package com.qanairy.config;
 
 import static com.qanairy.config.SpringExtension.SpringExtProvider;
 
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import akka.actor.ActorSystem;
 
@@ -21,13 +25,32 @@ public class ConfigService {
     private ApplicationContext applicationContext;
 	
 	/**
-	   * Actor system singleton for this application.
-	   */
-	  @Bean
-	  public ActorSystem actorSystem() {
-	    ActorSystem system = ActorSystem.create("QanairyActorSystem1");
-	    // initialize the application context in the Akka Spring Extension
-	    SpringExtProvider.get(system).initialize(applicationContext);
-	    return system;
-	  }
+	 * Actor system singleton for this application.
+	 */
+	@Bean
+	public ActorSystem actorSystem() {
+	  ActorSystem system = ActorSystem.create("QanairyActorSystem1");
+	  // initialize the application context in the Akka Spring Extension
+	  SpringExtProvider.get(system).initialize(applicationContext);
+	  return system;
+	}
+	
+
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
+	     
+	    mailSender.setUsername("my.gmail@gmail.com");
+	    mailSender.setPassword("password");
+	     
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.debug", "true");
+	     
+	    return mailSender;
+	}
 }
---
+++ src/main/java/com/qanairy/services/EmailServiceImpl.java
@@ -0,0 +1,38 @@
+package com.qanairy.services;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class EmailServiceImpl {
+  
+    @Autowired
+    public JavaMailSender emailSender;
+ 
+    public void sendSimpleMessage(
+      String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage(); 
+        message.setTo(to); 
+        message.setSubject(subject); 
+        message.setText(text);
+        emailSender.send(message);
+    }
+}

c084b3d6ac640afea604ecb9e46d5942e47668c4
Brandon Kindred <brandon.kindred@gmail.com>
2019-02-09 16:01:03 -0500

    refactored code to reduce complexity of createReceive

--- src/main/java/com/minion/actors/TestCreationActor.java
+++ src/main/java/com/minion/actors/TestCreationActor.java
@@ -1,13 +1,18 @@
 package com.minion.actors;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.openqa.grid.common.exception.GridException;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,6 +47,9 @@ import akka.cluster.ClusterEvent.MemberRemoved;
 import akka.cluster.ClusterEvent.MemberUp;
 import akka.cluster.ClusterEvent.UnreachableMember;
 
+/**
+ * Creates new tests based on {@link JSONOBject} containing test steps as defined by the plugin 
+ */
 @Component
 @Scope("prototype")
 public class TestCreationActor extends AbstractActor  {
@@ -93,9 +101,7 @@ public class TestCreationActor extends AbstractActor  {
 				    	}
 				    	
 				    	int attempts = 0;
-				    	Test test = null;
-				    	boolean first_page = true;
-				    	
+				    	Test test = null;				    	
 				    	Domain domain = null;
 				    			
 				    	do{
@@ -104,81 +110,9 @@ public class TestCreationActor extends AbstractActor  {
 					    	Browser browser = new Browser("chrome");
 
 				    		try{
-						    	for(int idx=0; idx < path.length(); idx++){
-						        	JSONObject path_obj_json = new JSONObject(path.get(idx).toString());
-						
-						    		if(path_obj_json.has("url")){
-						    			System.err.println("PATH OBJECT IS A URL :: " + path_obj_json);
-						    			String url = path_obj_json.getString("url");
-						    			if(first_page){
-						    				System.err.println("NAVIGATING TO URL :: " + url);
-						    				browser.navigateTo(url);
-						    				String host = new URL(url).getHost();
-
-						    				domain = domain_repo.findByHost(host);
-						    				first_page = false;
-						    			}
-						    			
-						    			//construct a new page
-						    			PageState page_state = browser_service.buildPage(browser);
-						    			path_keys.add(page_state.getKey());
-						    			path_objects.add(page_state);
-						    		}
-						    		else {
-						    			System.err.println("ELEMENT IN JSON :: " + path_obj_json.getJSONObject("element").toString());
-						    			JSONObject element_json = path_obj_json.getJSONObject("element");
-						    			//use xpath to identify WebElement. 
-						    			WebElement element = browser.findWebElementByXpath(element_json.getString("xpath"));
-						    			//use WebElement to generate system usable xpath
-						    			Set<Attribute> attributes = browser_service.extractAttributes(element, browser.getDriver());
-						    			String screenshot_url = browser_service.retrieveAndUploadBrowserScreenshot(browser.getDriver(), element);
-						
-						    			String xpath = browser_service.generateXpath(element, "", new HashMap<String, Integer>(), browser.getDriver(), attributes);
-						    			PageElement elem = new PageElement(element.getText(), xpath, element.getTagName(), attributes, Browser.loadCssProperties(element), screenshot_url);
-						    			PageElement elem_record = page_element_repo.findByKey(elem.getKey());
-						    			
-						    			if(elem_record != null){
-						    				elem = elem_record;
-						    			}
-						    			
-						    			//add to path
-						    			path_keys.add(elem.getKey());
-						    			path_objects.add(elem);
-
-						    			System.err.println("ACTION IN JSON ::  " + path_obj_json.getJSONObject("action"));
-						    			JSONObject action_json = path_obj_json.getJSONObject("action");
-						    			//create new action
-						    			//add action to Test
-						    			String action_type = action_json.getString("name");
-						    			String action_value = action_json.getString("value");
-						    			Action action = new Action(action_type, action_value);
-						    			Action action_record = action_repo.findByKey(action.getKey());
-						    			if(action_record != null){
-						    				action = action_record;
-						    			}
-						    			
-						    			path_keys.add(action.getKey());
-						    			path_objects.add(action);
-						    			Crawler.performAction(action, elem, browser.getDriver());
-						    			Timing.pauseThread(5000L);    			
-						    			
-						    			//******************************************************
-						    			// CHECK IF NEXT OBJECT IS  A URL BEFORE EXECUTING NEXT STEP. 
-						    			// IF NEXT OBJECT DOESN'T CONTAIN A URL, THEN CREATE NEW PAGE STATE
-						    			//******************************************************
-							        	if(idx+1 < path.length()){
-							    			path_obj_json = new JSONObject(path.get(idx+1).toString());
-					
-							    			if(!path_obj_json.has("url")){
-								    			//capture new page state and add it to path
-								    			PageState page_state = browser_service.buildPage(browser);
-								    			path_keys.add(page_state.getKey());
-								    			path_objects.add(page_state);
-							    			}
-							        	}
-						    		}
-						    	}
-						    	PageState result_page = browser_service.buildPage(browser);
+				    			buildTestPathFromPathJson(path, path_keys, path_objects, domain, browser);
+						    	
+				    			PageState result_page = browser_service.buildPage(browser);
 						    	test = new Test(path_keys, path_objects, result_page, name);
 						    	test.setStatus(TestStatus.PASSING);
 						    	test.getBrowserStatuses().put("chrome", TestStatus.PASSING.toString());
@@ -198,7 +132,6 @@ public class TestCreationActor extends AbstractActor  {
 				    		catch(Exception e){
 				    			log.warn("Error occurred while creating new test from IDE ::  "+e.getLocalizedMessage());
 				    			e.printStackTrace();
-								first_page = true;
 				    			browser.close();
 				    		}
 				    		attempts++;
@@ -222,5 +155,125 @@ public class TestCreationActor extends AbstractActor  {
 					log.info("received unknown message");
 				})
 				.build();
+	}
+
+	private void buildTestPathFromPathJson(JSONArray path, List<String> path_keys, List<PathObject> path_objects, Domain domain, Browser browser) throws JSONException, Exception {
+		boolean first_page = true;
+		for(int idx=0; idx < path.length(); idx++){
+        	JSONObject path_obj_json = new JSONObject(path.get(idx).toString());
+
+    		if(path_obj_json.has("url")){
+    			System.err.println("PATH OBJECT IS A URL :: " + path_obj_json);
+    			String url = path_obj_json.getString("url");
+    			String host = new URL(url).getHost();
+    			domain = domain_repo.findByHost(host);
+
+    			if(!first_page){
+    				PageState page_state = browser_service.buildPage(browser);
+    				path_keys.add(page_state.getKey());
+	    			path_objects.add(page_state);
+    			}
+    			
+    			PageState page_state = navigateToAndCreatePageState(url, first_page, browser);
+    			JSONObject action_json = path_obj_json.getJSONObject("action");
+
+    			String action_type = action_json.getString("name");
+    			String action_value = action_json.getString("value");
+    			
+    			Action action = new Action(action_type, action_value);
+    			Action action_record = action_repo.findByKey(action.getKey());
+    			if(action_record != null){
+    				action = action_record;
+    			}
+    			first_page = false;
+    			path_keys.add(page_state.getKey());
+    			path_objects.add(page_state);
+    		}
+    		else {
+    			System.err.println("ELEMENT IN JSON :: " + path_obj_json.getJSONObject("element").toString());
+    			JSONObject element_json = path_obj_json.getJSONObject("element");
+    			
+    			PageElement element = createPageElement(element_json.getString("xpath"), browser);
+    			//add to path
+    			path_keys.add(element.getKey());
+    			path_objects.add(element);
+
+    			System.err.println("ACTION IN JSON ::  " + path_obj_json.getJSONObject("action"));
+    			JSONObject action_json = path_obj_json.getJSONObject("action");
+    			
+    			//create new action
+    			//add action to Test
+    			String action_type = action_json.getString("name");
+    			String action_value = action_json.getString("value");
+    			
+    			Action action = createAction(action_type, action_value);
+    			path_keys.add(action.getKey());
+    			path_objects.add(action);
+    			
+    			Crawler.performAction(action, element, browser.getDriver());
+    			Timing.pauseThread(5000L);    			
+    			
+    			//******************************************************
+    			// CHECK IF NEXT OBJECT IS  A URL BEFORE EXECUTING NEXT STEP. 
+    			// IF NEXT OBJECT DOESN'T CONTAIN A URL, THEN CREATE NEW PAGE STATE
+    			//******************************************************
+	        	if(idx+1 < path.length()){
+	    			path_obj_json = new JSONObject(path.get(idx+1).toString());
+
+	    			if(!path_obj_json.has("url")){
+		    			//capture new page state and add it to path
+		    			PageState page_state = browser_service.buildPage(browser);
+		    			path_keys.add(page_state.getKey());
+		    			path_objects.add(page_state);
+	    			}
+	        	}
+    		}
+    	}
+	}
+
+	private Action createAction(String action_type, String action_value) {
+		Action action = new Action(action_type, action_value);
+		Action action_record = action_repo.findByKey(action.getKey());
+		if(action_record != null){
+			action = action_record;
+		}
+		
+		return action;
+	}
+
+	private PageElement createPageElement(String temp_xpath, Browser browser) throws Exception {
+		//use xpath to identify WebElement. 
+		WebElement element = browser.findWebElementByXpath(temp_xpath);
+		//use WebElement to generate system usable xpath
+		Set<Attribute> attributes = browser_service.extractAttributes(element, browser.getDriver());
+		String screenshot_url = browser_service.retrieveAndUploadBrowserScreenshot(browser.getDriver(), element);
+
+		String xpath = browser_service.generateXpath(element, "", new HashMap<String, Integer>(), browser.getDriver(), attributes);
+		PageElement elem = new PageElement(element.getText(), xpath, element.getTagName(), attributes, Browser.loadCssProperties(element), screenshot_url);
+		PageElement elem_record = page_element_repo.findByKey(elem.getKey());
+		
+		if(elem_record != null){
+			elem = elem_record;
+		}
+		return elem;
+	}
+
+	/**
+	 * Navigates to url in the given browser
+	 * 
+	 * @param url {@link String} value of {@link URL} object
+	 * @param isFirstPage 
+	 * @param browser
+	 * @return
+	 * @throws GridException
+	 * @throws NoSuchAlgorithmException
+	 * @throws IOException
+	 */
+	private PageState navigateToAndCreatePageState(String url, boolean isFirstPage, Browser browser) 
+									throws GridException, NoSuchAlgorithmException, IOException {
+		browser.navigateTo(url);
+		
+		//construct a new page
+		return browser_service.buildPage(browser);
 	}
 }

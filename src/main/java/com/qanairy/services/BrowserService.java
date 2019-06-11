51dea773fa6365ce17eb3faab74d1db4a293bad0
Brandon Kindred <brandon.kindred@gmail.com>
2019-06-10 21:19:28 -0400

    added try catch logic around page build when building page states (#358)

--- src/main/java/com/qanairy/services/BrowserService.java
+++ src/main/java/com/qanairy/services/BrowserService.java
@@ -159,50 +159,53 @@ public class BrowserService {
 				web_elements = BrowserService.filterElementsWithNegativePositions(web_elements);
 
 				all_elements = new ArrayList<>(web_elements);
-				log.warn("total elements without seen elements filtered  :: " + all_elements);
+				log.warn("total elements without seen elements filtered  :: " + all_elements.size());
 				web_elements = BrowserService.filterElementStatesFromList(web_elements, new ArrayList<ElementState>(seen_element_state.values()) );
 
 				int iter_idx=0;
 				int idx = 0;
-				while(all_elements.size() > element_hash.keySet().size()){		
-					log.warn("run : " + idx + ";    init browser scroll offsets  ::   "+browser.getXScrollOffset()+" : "+browser.getYScrollOffset());
-					log.warn("run : " + idx + ";    BrowserService ...identifying page state iteration ...."+idx+" elements remaining ...."+web_elements.size() + "    :    " + url);
-					idx++;
-					if(iter_idx > 1){
-						web_elements = web_elements.subList(1, web_elements.size());
-						iter_idx=0;
+				while(all_elements.size() > element_hash.keySet().size()){	
+					try{
+						log.warn("run : " + idx + ";    init browser scroll offsets  ::   "+browser.getXScrollOffset()+" : "+browser.getYScrollOffset());
+						log.warn("run : " + idx + ";    BrowserService ...identifying page state iteration ...."+idx+" elements remaining ...."+web_elements.size() + "    :    " + url);
+						idx++;
+						if(iter_idx > 1){
+							web_elements = web_elements.subList(1, web_elements.size());
+							iter_idx=0;
+						}
+						if(web_elements.isEmpty()){
+							break;
+						}
+						if(!isElementVisibleInPane(browser, web_elements.get(0)) || iter_idx > 0){
+							log.warn("run : " + idx + ";    element is not visible in pane :: " + web_elements.get(0).getLocation()  + "    :    " + url);
+							browser.scrollToElement(web_elements.get(0));
+						}
+						log.warn("run : " + idx + ";    browser scroll offsets  ::   "+browser.getXScrollOffset()+" : "+browser.getYScrollOffset());
+	
+						PageState page_state = buildPage(browser, all_elements);
+						page_states.add(page_state);
+	
+						log.warn("Page state element count ::  " + page_state.getElements().size() + "   :   "+ url);
+						/*
+						Animation animation = BrowserUtils.getAnimation(browser, host);
+						log.warn("setting animated image urls :: " + animation.getImageUrls().size());
+						page_state.setAnimatedImageUrls(animation.getImageUrls());
+		*/
+						log.warn("run : " + idx + ";    elements left to go :: " + web_elements.size() + "    :    " + url);
+						for(ElementState element : page_state.getElements()){
+							element_hash.put(element.getXpath(), element);
+						}
+						
+						web_elements = BrowserService.filterElementStatesFromList(web_elements, page_state.getElements());
+						
+						log.warn("run : " + idx + ";    seen states :: " + seen_element_state.size() + "    :    " + url);
+						log.warn("run : " + idx + ";    elements left after filtered list :: " + web_elements.size() + "    :    " + url);
+						
+						iter_idx++;
 					}
-					if(web_elements.isEmpty()){
-						break;
+					catch(RasterFormatException e){
+						log.warn("element is not within raster :: "+e.getMessage());
 					}
-					if(!isElementVisibleInPane(browser, web_elements.get(0)) || iter_idx > 0){
-						log.warn("run : " + idx + ";    element is not visible in pane :: " + web_elements.get(0).getLocation()  + "    :    " + url);
-						browser.scrollToElement(web_elements.get(0));
-					}
-					log.warn("run : " + idx + ";    browser scroll offsets  ::   "+browser.getXScrollOffset()+" : "+browser.getYScrollOffset());
-
-					PageState page_state = buildPage(browser, all_elements);
-					page_states.add(page_state);
-
-					log.warn("Page state element count ::  " + page_state.getElements().size() + "   :   "+ url);
-					/*
-					Animation animation = BrowserUtils.getAnimation(browser, host);
-					log.warn("setting animated image urls :: " + animation.getImageUrls().size());
-					page_state.setAnimatedImageUrls(animation.getImageUrls());
-	*/
-					log.warn("run : " + idx + ";    elements left to go :: " + web_elements.size() + "    :    " + url);
-					for(ElementState element : page_state.getElements()){
-						element_hash.put(element.getXpath(), element);
-					}
-					
-					web_elements = BrowserService.filterElementStatesFromList(web_elements, page_state.getElements());
-					
-					log.warn("run : " + idx + ";    seen states :: " + seen_element_state.size() + "    :    " + url);
-					
-					log.warn("run : " + idx + ";    elements left after filtered list :: " + web_elements.size() + "    :    " + url);
-					
-					
-					iter_idx++;
 				}
 				error_occurred = false;
 				break;

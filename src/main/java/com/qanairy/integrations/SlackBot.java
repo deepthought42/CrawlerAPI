10b731fc8e620bc3b88ed734fec43a608dc98aa6
Brandon Kindred <brandon.kindred@gmail.com>
2019-07-14 02:06:06 -0400

    jslack upgrade

--- src/main/java/com/qanairy/integrations/SlackBot.java
+++ src/main/java/com/qanairy/integrations/SlackBot.java
@@ -1,43 +1,41 @@
 package com.qanairy.integrations;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.WebSocketSession;
 
-import me.ramswaroop.jbot.core.slack.Bot;
-import me.ramswaroop.jbot.core.slack.models.Attachment;
-import me.ramswaroop.jbot.core.slack.models.Event;
-import me.ramswaroop.jbot.core.slack.models.Message;
-import me.ramswaroop.jbot.core.slack.models.RichMessage;
+import com.github.seratch.jslack.*;
+import com.github.seratch.jslack.api.webhook.*;
+
+import okhttp3.Response;
 
 
 @Component
-public class SlackBot extends Bot {
+public class SlackBot {
     
 	private static Logger log = LoggerFactory.getLogger(SlackBot.class);
 
     @Value("${slackBotToken}")
     private String slackToken;
 
-    @Override
+    
     public String getSlackToken() {
         return slackToken;
     }
-
-    @Override
-    public Bot getSlackBot() {
-        return this;
-    }
     
-    public void sendMessage(String channel_id, String message) {
-        /** build response */
-        RichMessage richMessage = new RichMessage("The is Slash Commander!");
-        richMessage.setResponseType("in_channel");
-        // set attachments
-        richMessage.setChannel(channel_id);
-        richMessage.setText(message);
-        sendMessage(channel_id, message);
+    public void sendMessage(String channel_id, String message) throws IOException {
+    	// https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX
+    	String url = System.getenv("SLACK_WEBHOOK_URL");
+
+    	Payload payload = Payload.builder()
+    	  .text(message)
+    	  .build();
+
+    	Slack slack = Slack.getInstance();
+    	Response response = slack.send(url, payload);
+    	// response.code, response.message, response.body
     }
 }

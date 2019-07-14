a0ab6b91df75e182874a876fbf936a8cb0349f95
Brandon Kindred <brandon.kindred@gmail.com>
2019-07-14 01:01:15 -0400

    changed to jslack

---
+++ src/main/java/com/minion/api/integration/SlackController.java
@@ -0,0 +1,64 @@
+package com.minion.api.integration;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.minion.api.exception.InvalidApiKeyException;
+import com.qanairy.integrations.SlackBot;
+import com.qanairy.models.Account;
+import com.qanairy.models.repository.AccountRepository;
+
+
+@RestController
+@RequestMapping("/integration/slack")
+public class SlackController {
+	@Autowired
+	private AccountRepository account_repo;
+	
+	/**
+	 * Runs all tests for a given domain and account using an api key to locate the account
+	 * In the event that the domain is not registered with the {@link Account} then the system throws
+	 * an exception
+	 * 
+	 * @param domain
+	 * @param api_key
+	 * 
+	 * @return
+	 * @throws InvalidApiKeyException 
+	 */
+    @RequestMapping(method = RequestMethod.GET)
+	public void slackAuthentication(HttpServletRequest request, 
+										@RequestParam String code,
+										@RequestParam String api_key) throws InvalidApiKeyException{
+    	Account acct = account_repo.getAccountByApiKey(api_key);
+		if(acct == null){
+    		throw new InvalidApiKeyException("Invalid API key");
+    	}
+    	
+		SlackBot bot = new SlackBot();
+		bot.getSlackBot();
+		/* UNCOMMENT WHEN READY TO HANDLE SUBSCRIPTIONS
+		 
+    	if(subscription_service.hasExceededSubscriptionTestRunsLimit(acct, subscription_service.getSubscriptionPlanName(acct))){
+    		throw new PaymentDueException("Your plan has 0 test runs available. Upgrade now to run more tests");
+        }
+    	*/
+		
+		Date start_date = new Date();
+		long start = System.currentTimeMillis();
+    	long end = System.currentTimeMillis();
+    	
+    	long time_in_sec = (end-start)/1000;
+    	
+    	
+	}
+}

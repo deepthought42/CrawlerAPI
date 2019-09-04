d177ed756548809f79c49c7f577402ab84ba6539
Brandon Kindred <brandon.kindred@gmail.com>
2019-09-03 20:34:26 -0400

    more changes for quartz...nothin works though

--- src/main/java/com/minion/api/EntryPoint.java
+++ src/main/java/com/minion/api/EntryPoint.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.neo4j.Neo4jDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
@@ -17,14 +18,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  *
  */
 
-@SpringBootApplication(exclude={Neo4jDataAutoConfiguration.class, SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude={Neo4jDataAutoConfiguration.class, SecurityAutoConfiguration.class, QuartzAutoConfiguration.class  })
 @ComponentScan(basePackages = {"com.minion","com.qanairy"})
 @PropertySources({
+	@PropertySource("classpath:quartz.properties"),
 	@PropertySource("classpath:application.properties"),
 	@PropertySource("classpath:auth0.properties")
 })
-@EnableAutoConfiguration(exclude = { FreeMarkerAutoConfiguration.class, DataSourceAutoConfiguration.class })
 @EnableScheduling
+@EnableAutoConfiguration(exclude = { FreeMarkerAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class EntryPoint {
 	
 	public static void main(String[] args){

1c964202edc3303f1169ca364e0e1a72ad34dc7c
Brandon Kindred <brandon.kindred@gmail.com>
2024-04-14 21:38:39 -0400

    updated security config

--- src/main/java/com/looksee/security/SecurityConfig.java
+++ src/main/java/com/looksee/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.looksee.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,7 +14,7 @@ public class SecurityConfig {
 	
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .mvcMatchers("/actuator/info").permitAll()
                 .mvcMatchers( "/actuator/health").permitAll()
                 .mvcMatchers( "/auditor/start-individual").permitAll()
@@ -22,16 +23,16 @@ public class SecurityConfig {
                 .and().oauth2ResourceServer().jwt();
         return http.build();
     }
-    /*
 	@Value(value = "${auth0.domain}")
 	private String domain;
     
     @Value("${auth0.audience}")
     private String audience;
-
+    
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     private String issuer;
-
+    
+    /*
     @Bean
     public UserDetailsService userDetailsServiceBean() throws Exception {
         return super.userDetailsServiceBean();
@@ -67,14 +68,14 @@ public class SecurityConfig {
         
         OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
         OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
-        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
-
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator(null)<>(withIssuer, audienceValidator);
+        
         jwtDecoder.setJwtValidator(withAudience);
-
+        
         return jwtDecoder;
     }
-    
     */
+    
     @Bean
 	CorsConfigurationSource corsConfigurationSource() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();

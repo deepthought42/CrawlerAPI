0baed4924b4d51d56cc734d2d66e5be43ee7d1ec
Brandon Kindred <brandon.kindred@gmail.com>
2025-09-04 01:02:26 -0400

    chore: rebase fixes

--- src/main/java/com/crawlerApi/config/Auth0Config.java
+++ src/main/java/com/crawlerApi/config/Auth0Config.java
@@ -2,12 +2,10 @@ package com.crawlerApi.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 import lombok.Getter;
 import lombok.Setter;
 
-@Component
 @Configuration
 @ConfigurationProperties(prefix = "auth0")
 @Getter
@@ -26,28 +24,92 @@ public class Auth0Config {
     private boolean defaultAuth0ApiSecurityEnabled;
     private String signingAlgorithm;
     
-    // Additional methods for user information retrieval
-    public String getAuth0Domain() {
+    // Getters and Setters
+    public String getDomain() {
         return domain;
     }
     
-    public String getAuth0Issuer() {
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+    
+    public String getIssuer() {
         return issuer;
     }
     
-    public String getAuth0ClientId() {
+    public void setIssuer(String issuer) {
+        this.issuer = issuer;
+    }
+    
+    public String getApiAudience() {
+        return apiAudience;
+    }
+    
+    public void setApiAudience(String apiAudience) {
+        this.apiAudience = apiAudience;
+    }
+    
+    public String getClientId() {
         return clientId;
     }
     
-    public String getAuth0ClientSecret() {
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+    
+    public String getClientSecret() {
         return clientSecret;
     }
     
-    public String getAuth0Audience() {
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+    
+    public String getAudience() {
         return audience;
     }
     
-    public String getAuth0ApiAudience() {
-        return apiAudience;
+    public void setAudience(String audience) {
+        this.audience = audience;
+    }
+    
+    public String getSecuredRoute() {
+        return securedRoute;
+    }
+    
+    public void setSecuredRoute(String securedRoute) {
+        this.securedRoute = securedRoute;
+    }
+    
+    public boolean isBase64EncodedSecret() {
+        return base64EncodedSecret;
+    }
+    
+    public void setBase64EncodedSecret(boolean base64EncodedSecret) {
+        this.base64EncodedSecret = base64EncodedSecret;
+    }
+    
+    public String getAuthorityStrategy() {
+        return authorityStrategy;
+    }
+    
+    public void setAuthorityStrategy(String authorityStrategy) {
+        this.authorityStrategy = authorityStrategy;
+    }
+    
+    public boolean isDefaultAuth0ApiSecurityEnabled() {
+        return defaultAuth0ApiSecurityEnabled;
+    }
+    
+    public void setDefaultAuth0ApiSecurityEnabled(boolean defaultAuth0ApiSecurityEnabled) {
+        this.defaultAuth0ApiSecurityEnabled = defaultAuth0ApiSecurityEnabled;
+    }
+    
+    public String getSigningAlgorithm() {
+        return signingAlgorithm;
+    }
+    
+    public void setSigningAlgorithm(String signingAlgorithm) {
+        this.signingAlgorithm = signingAlgorithm;
     }
 } 
